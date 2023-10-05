a31e36bcf2133d8cc7324a76bac88d7f73941bf4
IdiotPlumms <144438517+IdiotPlumms@users.noreply.github.com>
2023-10-05 13:40:29 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,45 @@
+print("Who is the first volunteer?")
+Person_01 = input()
+print("What coin did they count?")
+Cointype_01 = input()
+
+print("Who is the second volunteer?")
+Person_02 = input()
+print("What coin did they count?")
+Cointype_02 = input()
+
+print("Who is the thrid volunteer?")
+Person_03 = input()
+print("What coin did they count?")
+Cointype_03 = input()
+
+print("Who is the fourth Volunteer?")
+Person_04 = input()
+print("What coin did they count?")
+Cointype_04 = input()
+
+print("Who is the Fith Volunteer?")
+Person_05 = input()
+print("What coin did they count?")
+Cointype_05 = input()
+
+print("Who is the Final Volunteer?")
+Person_06 = input()
+print("What coin did they count?")
+Cointype_06 = input()
+
+print("Would you like a list of the volunteers")
+answer = input()
+if answer == 'yes' or 'Yes':
+
+    print(Person_01, Cointype_01)
+    print(Person_02, Cointype_02)
+    print(Person_03, Cointype_03)
+    print(Person_04, Cointype_04)
+    print(Person_05, Cointype_05)
+    print(Person_06, Cointype_06)
+else:
+    print("Alright Fair Enough")
+    
+
+
